6a97c16935cecf406b7928d9008c7b91672f4455
Abbas Elmas <abbaselmas@gmail.com>
2021-01-24 16:09:46 +0300

    photo updated

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   url: 'https://abbaselmas.com',
   pathPrefix: '/',
   title: 'Blog by Abbas Elmas',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  subtitle: 'junk site of a tech savvy geek',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -12,26 +12,26 @@ module.exports = {
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
+      label: 'Posts',
       path: '/'
     },
     {
-      label: 'About me',
+      label: 'About',
       path: '/pages/about'
     },
     {
-      label: 'Contact me',
+      label: 'Contact',
       path: '/pages/contacts'
     }
   ],
   author: {
     name: 'Abbas Elmas',
-    photo: '/photo.jpg',
+    photo: '/photo.png',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
       email: 'abbas1173@gmail.com',
       facebook: '',
-      telegram: '',
+      telegram: 'abbaselmas',
       twitter: 'aabbstr',
       github: 'abbaselmas',
       rss: '',
